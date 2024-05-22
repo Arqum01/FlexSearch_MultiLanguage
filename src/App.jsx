6a4dc1ad65f770c495a7e2cab3099a6fe475b334
Arqum01 <arqumahmad019@gmail.com>
2024-05-22 13:15:59 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import './App.css';
+import IndoPak13 from './Components/IndoPak13';
+
+function App() {
+
+  return (
+    <>
+      <IndoPak13/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/IndoPak13.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect, useMemo } from "react";
+import translation from './Translation.json';
+import translation4 from './Translation4.json';
+import FlexSearch from 'flexsearch';
+
+export default function IndoPak13() {
+
+  const [trans1, setTrans1] = useState(true);
+  const [trans2, setTrans2] = useState(false);
+  const [inputText, setInputText] = useState('');
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handlePress = (text) => {
+    if (text === 'Translation1') {
+      setTrans1(true);
+      setTrans2(false);
+      setQuery("");
+      setInputText("");
+    } else {
+      setTrans2(true);
+      setTrans1(false);
+      setQuery("");
+      setInputText("");
+    }
+  };
+
+  const DataTranslation = trans1 ? translation : translation4;
+
+  const index = useMemo(() => new FlexSearch({
+    encode: "icase",
+    split: /\s+/,
+    tokenize: 'full',
+  }), []);
+
+  useEffect(() => {
+    DataTranslation.forEach((Translation, idx) => {
+      index.add(idx, `${Translation}`);
+    });
+  }, [index, DataTranslation]);
+
+  useEffect(() => {
+    if (query === '') {
+      setResults([]);
+    } else {
+      const searchResults = index.search(query, { limit: 10 });
+      setResults(searchResults.map(i => ({ translation: DataTranslation[i] })));
+    }
+  }, [query, index, DataTranslation]);
+
+  const handleQueryChange = e => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setQuery(inputText);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <button onClick={() => handlePress('Translation1')} className={trans1 ? 'active' : ''}>
+          Translation1
+        </button>
+        <button onClick={() => handlePress('Translation2')} className={trans2 ? 'active' : ''}>
+          Translation2
+        </button>
+      </div>
+      <form onSubmit={handleSubmit} className="input-form">
+        <input
+          className="input"
+          placeholder="Search"
+          value={inputText}
+          onChange={handleQueryChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="subcontainer">
+        {results.length > 0 ? (
+          results.map((item, index) => (
+            <div key={index} className="textContainer">
+              {/* <p className="TextData">
+                {item.arabic}
+              </p> */}
+              <p className="TranslationData">
+                {item.translation}
+              </p>
+            </div>
+          ))
+        ) : (
+            DataTranslation.map((item, index) => (
+            <div key={index} className="textContainer">
+              {/* <p className="TextData">
+                {item}
+              </p> */}
+              <p className="TranslationData">
+                {DataTranslation[index]}
+              </p>
+            </div>
+          ))
+        )}
+      </div>
+    </>
+  );
+}
